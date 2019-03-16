2bd8d60f42575ddfba195d0bc89dbc2249a28bff
jonp <bignewelly@gmail.com>
2019-03-16 14:34:11 -0600

    Update Canvas.xaml.cs
    Add the ability to add rectangles of different sizes.

--- WpfApplication1/Canvas.xaml.cs
+++ WpfApplication1/Canvas.xaml.cs
@@ -26,67 +26,96 @@ namespace BasicGeometryApp
 
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            System.Windows.Controls.Canvas canvas = GetCanvas();
+            System.Windows.Controls.Canvas canvas = Canvas_Get();
 
-            MouseStartPoint = e.GetPosition(canvas);
-            MouseEndPoint = e.GetPosition(canvas);
+            LastMouseLocation = e.GetPosition(canvas);
 
-            Transform transform = new TranslateTransform(MouseStartPoint.X, MouseStartPoint.Y);
+            Transform transform = new TranslateTransform(LastMouseLocation.Value.X, LastMouseLocation.Value.Y);
 
             Canvas_ReDraw(transform);
         }
 
-        private void Canvas_ReDraw(Transform transform)
-        {
-            RectangleGeometry rectGeo = GetRectangleGeometry();
-
-            rectGeo.Transform = transform;
-
-            //rectGeo.Bounds.TopLeft = MouseStartPoint;
-        }
-
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
+            Point currentMouseLocation = e.GetPosition(Canvas_Get());
 
+            TransformGroup transfromGroup = new TransformGroup();
+            transfromGroup.Children.Add(ScaleTransform_Get(currentMouseLocation));
+            transfromGroup.Children.Add(TranslateTransform_Get(currentMouseLocation));
+
+            LastMouseLocation = null;
+
+            Canvas_ReDraw(transfromGroup);
         }
 
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
+            if (LastMouseLocation != null)
+            {
+                Point currentMouseLocation = e.GetPosition(Canvas_Get());
+
+                TransformGroup transfromGroup = new TransformGroup();
+                transfromGroup.Children.Add(ScaleTransform_Get(currentMouseLocation));
+                transfromGroup.Children.Add(TranslateTransform_Get(currentMouseLocation));
+
+                Canvas_ReDraw(transfromGroup);
+            }
+        }
+
+        private Transform ScaleTransform_Get(Point CurrentPoint)
+        {
+            double diffX = Math.Abs(CurrentPoint.X - LastMouseLocation.Value.X);
+            double diffY = Math.Abs(CurrentPoint.Y - LastMouseLocation.Value.Y);
+
+            return new ScaleTransform(diffX, diffY);
+        }
+
+        private Transform TranslateTransform_Get(Point CurrentPoint)
+        {
+            double locX = Math.Min(LastMouseLocation.Value.X, CurrentPoint.X);
+            double locY = Math.Min(LastMouseLocation.Value.Y, CurrentPoint.Y);
+
+            return new TranslateTransform(locX, locY);
+        }
+
+        private void Canvas_ReDraw(Transform transform)
+        {
+            RectangleGeometry rectGeo = RectangleGeometry_Get();
+
+            rectGeo.Transform = transform;
 
         }
 
-        private RectangleGeometry GetRectangleGeometry()
+        private RectangleGeometry RectangleGeometry_Get()
         {
-            RectangleGeometry rectGeo = (RectangleGeometry)this.FindName("rectGeo");
-
-            if (rectGeo == null)
+            if (RectGeo == null)
             {
-                System.Windows.Controls.Canvas drawingCanvas = GetCanvas();
+                System.Windows.Controls.Canvas drawingCanvas = Canvas_Get();
 
-                rectGeo = new RectangleGeometry(new Rect(new Point(0, 0), new Point(1, 1)));
+                RectGeo = new RectangleGeometry(new Rect(new Point(0, 0), new Point(1, 1)));
 
                 GeometryGroup group = new GeometryGroup();
-                group.Children.Add(rectGeo);
+                group.Children.Add(RectGeo);
 
                 Path rectPath = new Path();
 
                 rectPath.Stroke = Brushes.Black;
                 rectPath.StrokeThickness = 1;
                 rectPath.Fill = Brushes.Aqua;
-                rectPath.Data = rectGeo;
+                rectPath.Data = RectGeo;
 
                 drawingCanvas.Children.Add(rectPath);
             }
 
-            return rectGeo;
+            return RectGeo;
         }
 
-        private System.Windows.Controls.Canvas GetCanvas()
+        private System.Windows.Controls.Canvas Canvas_Get()
         {
             return (System.Windows.Controls.Canvas)this.FindName("drawingCanvas");
         }
 
-        protected Point MouseStartPoint { get; set; }
-        protected Point MouseEndPoint { get; set; }
+        private Point? LastMouseLocation { get; set; }
+        private RectangleGeometry RectGeo { get; set;}
     }
 }
